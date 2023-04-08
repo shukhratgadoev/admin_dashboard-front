68eba23cf403ca98571c1d1171b65d232a8a87a7
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2023-04-08 19:48:37 +0500

    feat: Finish Customers page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -18,7 +18,7 @@ const Employees = () => {
       className="m-2 md:m-10 p-2 md:p-10 
     bg-white rounded-3xl"
     >
-      <Header category="Page" title="Orders" />
+      <Header category="Page" title="Employees" />
       <GridComponent
         dataSource={employeesData}
         allowPaging
